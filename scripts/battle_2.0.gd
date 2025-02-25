6a8bffbe9af0f7d5276173e43d6f6d5f605fd388
duskgame <jonas.hauschild.jh@gmail.com>
2025-02-25 23:24:21 +0100

    update main menu visual

--- scripts/battle_2.0.gd
+++ scripts/battle_2.0.gd
@@ -11,7 +11,7 @@ signal player_won
 @onready var enemy: Enemy = $Enemy
 @onready var camera: Camera2D = $Camera2D
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	add_to_group("active_battle")
 	assert(player)
