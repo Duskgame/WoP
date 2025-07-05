42be835993ad5a443e2593134ae8b465ad8928d7
duskgame <jonas.hauschild.jh@gmail.com>
2025-07-05 21:50:33 +0200

    ceil health

--- scripts/hp_bar.gd
+++ scripts/hp_bar.gd
@@ -7,6 +7,6 @@ class_name HPbar
 
 
 func set_health(health: float, max_health: float):
-	bar.value = health
+	bar.value = ceil(health)
 	bar.max_value = max_health
 	text.text = "HP: %d/%d" % [health,max_health]
